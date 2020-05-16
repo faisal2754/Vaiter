dc45bf73bb80203c147ef9560e40e79adc4aea2c
Ghulame Mujtaba Arbi <mujtaba.rbee091@gmail.com>
2020-05-16 23:10:54 +0200

    Test

--- app/src/main/java/com/example/vaiterapp/MainActivity.java
+++ app/src/main/java/com/example/vaiterapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.vaiterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.widget.LinearLayout;
@@ -19,5 +20,18 @@ public class MainActivity extends AppCompatActivity {
         TextView t = new TextView(this);
         t.setText("Nigga wtf");
         lin.addView(t);
+    }
+
+    protected void onStart() {
+        super.onStart();
+        SendUserToLoginActivity();
+    }
+
+    private void SendUserToLoginActivity() {
+        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        startActivity(loginIntent);
+        finish();
     }
 }
