38c7af0d14f41806c1a458143ab4ac90a765672d
Faisal <saleemfs27@gmail.com>
2020-05-16 19:51:51 +0200

    commit again

--- app/src/main/java/com/example/vaiterapp/MainActivity.java
+++ app/src/main/java/com/example/vaiterapp/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.vaiterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        LinearLayout lin = new LinearLayout(this);
+        lin.setGravity(Gravity.CENTER_HORIZONTAL);
+        setContentView(lin);
+        TextView t = new TextView(this);
+        t.setText("wtf lol");
+        lin.addView(t);
     }
 }
