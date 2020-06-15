327495e05803a0ccc85e0d931c10f5b176d9579c
Ghulame Mujtaba Arbi <mujtaba.rbee091@gmail.com>
2020-06-15 20:38:22 +0200

    Changed layout of spinner.

--- app/src/main/java/com/example/vaiterapp/StaffSignupActivity.java
+++ app/src/main/java/com/example/vaiterapp/StaffSignupActivity.java
@@ -3,15 +3,19 @@ package com.example.vaiterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class StaffSignupActivity extends AppCompatActivity {
@@ -24,19 +28,63 @@ public class StaffSignupActivity extends AppCompatActivity {
         Spinner staticSpinner = (Spinner) findViewById(R.id.spinnerRestaurant);
         txtAlreadyM.setOnClickListener(v -> LoginClick());
 
-        List<String> spinnerArray =  new ArrayList<String>();
-        spinnerArray.add("Select your restaurant");
-        spinnerArray.add("McDonalds");
-        spinnerArray.add("Ocean Basket");
+        final Spinner spinner = (Spinner) findViewById(R.id.spinnerRestaurant);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_item,spinnerArray);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        Spinner sItems = (Spinner) findViewById(R.id.spinnerRestaurant);
-        sItems.setAdapter(adapter);
+        // Initializing a String Array
+        String[] plants = new String[]{
+                "Select your restaurant...",
+                "McDonalds",
+                "Ocean Basket"
+        };
 
-//        String selected = sItems.getSelectedItem().toString();
-//            if (selected.equals("what ever the option was")) {
-//        }
+        final List<String> plantsList = new ArrayList<>(Arrays.asList(plants));
+
+        final ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(
+                this,R.layout.spinner_item,plantsList){
+            @Override
+            public boolean isEnabled(int position){
+                if(position == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if(position == 0){
+                    tv.setTextColor(Color.GRAY);
+                }
+                else {
+                    tv.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+        };
+        spinnerArrayAdapter.setDropDownViewResource(R.layout.spinner_item);
+        spinner.setAdapter(spinnerArrayAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItemText = (String) parent.getItemAtPosition(position);
+                if(position > 0){
+                    Toast.makeText
+                            (getApplicationContext(), "Selected: " + selectedItemText, Toast.LENGTH_SHORT)
+                            .show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
     }
 
