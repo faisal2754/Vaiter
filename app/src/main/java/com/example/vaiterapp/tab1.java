47a14e8c54740cc989170fa6dd14fcf28b7676b9
Faisal <saleemfs27@gmail.com>
2020-06-20 20:46:22 +0200

    working list view

--- app/src/main/java/com/example/vaiterapp/tab1.java
+++ app/src/main/java/com/example/vaiterapp/tab1.java
@@ -19,28 +19,39 @@ import androidx.fragment.app.Fragment;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Objects;
 import java.util.Set;
 
 public class tab1 extends Fragment {
     private View Tab1View;
     private ListView list_view;
-    private ArrayAdapter<String> arrayAdapter;
-    private ArrayList<String> list_of_groups = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> listItems;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState){
         View rootView = inflater.inflate(R.layout.tab1_customer_main,container,false);
 
-
-        TextView tv = (TextView) rootView.findViewById(R.id.section_label);
+        /*TextView tv = (TextView) rootView.findViewById(R.id.section_label);
         tv.setText("WHATSUP");
-        tv.setTextColor(Color.RED);
+        tv.setTextColor(Color.RED);*/
 
+        list_view = rootView.findViewById(R.id.list_view);
 
+        listItems=new ArrayList<String>();
 
+        adapter=new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1,
+                listItems);
+
+        list_view.setAdapter(adapter);
+
+        listItems.add("wtf");
+        listItems.add("lol");
 
         return rootView;
     }
+
 
 
 }
