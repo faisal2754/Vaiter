cf901634a85d11631e30f3dc36d89955a5d63b4f
Ghulame Mujtaba Arbi <mujtaba.rbee091@gmail.com>
2020-06-28 14:47:03 +0200

    Updated profile activity.

--- app/src/main/java/com/example/vaiterapp/ProfileActivity.java
+++ app/src/main/java/com/example/vaiterapp/ProfileActivity.java
@@ -10,7 +10,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.io.InputStream;
 
@@ -18,12 +20,37 @@ public class ProfileActivity extends AppCompatActivity {
 
     private static final int GALLERY_REQUEST_CODE = 123;
 
+    private String cussName = MainActivity.prf1.getString("fname", "");
+    private String email = MainActivity.prf1.getString("email", "");
+
+    private TextView tvName;
+    private TextView tvEmail;
+
+    private Button Save;
+
+
     ImageView ProfilePic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        tvName = findViewById(R.id.name_view);
+        tvEmail = findViewById(R.id.email_view);
+        Save = findViewById(R.id.btnSave);
+
+        tvName.setText(tvName.getText() + cussName);
+        tvEmail.setText(tvEmail.getText() + email);
+
+        Save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent staffIntent = new Intent(ProfileActivity.this, CustomerMainActivity.class);
+                startActivity(staffIntent);
+            }
+        });
+
 
 //        ProfilePic = (ImageView) findViewById(R.id.profile_pic);
 //
@@ -37,6 +64,7 @@ public class ProfileActivity extends AppCompatActivity {
 //
 //            }
 //        });
+        
     }
 
 //    @Override
